60a0324c602ffce5c50319f968f57622b33654d5
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-08 12:56:34 -0400

    iniciado proyecto

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RootApp());
+}
+
+class RootApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo'
+     
+    );
+  }
+
+
+}
+
+ 
